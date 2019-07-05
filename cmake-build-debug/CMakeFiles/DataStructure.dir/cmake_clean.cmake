7b93344483300fce7a37888b473923a124644c30
yang.li@dorabot.com <yang.li@dorabot.com>
2019-07-05 10:12:57 +0800

    clion

---
+++ cmake-build-debug/CMakeFiles/DataStructure.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructure.dir/src/main.cpp.o"
+  "../bin/DataStructure.pdb"
+  "../bin/DataStructure"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
